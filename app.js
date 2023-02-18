89c86a9f6845f462fa3ca48dd479a1a2efc175a0
Macpaper <macpaper23@yahoo.com>
2023-02-18 12:53:06 -0500

    thingy

---
+++ app.js
@@ -0,0 +1,21 @@
+import Game from "./Game.js";
+
+let canv = document.querySelector("canvas");
+let ctx = canv.getContext("2d");
+
+const WIDTH = (canv.width = window.innerWidth);
+const HEIGHT = (canv.height = window.innerHeight);
+
+let game = new Game(WIDTH, HEIGHT);
+
+function gameLoop() {
+  ctx.fillStyle = "rgb(50, 50, 50)";
+  ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+  game.update();
+  game.draw(ctx);
+
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
